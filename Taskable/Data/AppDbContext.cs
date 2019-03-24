53920296c101eefaad666a60ae5c7108bd287113
Grant Watson <grant@triumphdesigns.org>
2019-03-24 18:04:06 -0400

    initial commit

---
+++ Taskable/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Taskable.Models;
+
+namespace Taskable.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tasks> Tasks { get; set; }
+    }
+}
---
+++ Taskable/Models/Tasks.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Taskable.Models
+{
+    public class Tasks
+    {
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
